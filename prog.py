a4c65665c20c3af38dbc6e12821b36825f361f75
Roosie Kuloba <roosie.biz@gmail.com>
2023-01-13 14:58:04 +0300

    Simple GUI

--- prog.py
+++ prog.py
@@ -1,56 +1,59 @@
-# number buttons
-btn7=7;
-btn8=8;
-btn9=9;
-btn4=4;
-btn5=5;
-btn6=6;
-btn1=1;
-btn2=2;
-btn3=3;
-btn0=0;
+from tkinter import *
 
-# button to make a number a float
-btnPeriod=".";
+root = Tk()
+root.title("Roosiez Calculator")
 
-#button to delete the entire input
-btnDel="#";
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-#button to delete one unit of the input
-btnClr="#";
 
-#operational buttons
-btnMply="*";
-btnDivi="/";
-btnAdd="+";
-btnMinus="-";
+def button_add():
+    return
 
-#button to display the result
-btnEqual="#";
+# Define buttons
 
-#button to refer to the last result stored
-btnAns="#";
 
-print("Welcome to Roosiez Calculator! ");
-print("Let's begin.");
+button_1 = Button(root, text="1", padx=40, pady=20, command=button_add,)
+button_2 = Button(root, text="2", padx=40, pady=20, command=button_add,)
+button_3 = Button(root, text="3", padx=40, pady=20, command=button_add,)
+button_4 = Button(root, text="4", padx=40, pady=20, command=button_add,)
+button_5 = Button(root, text="5", padx=40, pady=20, command=button_add,)
+button_6 = Button(root, text="6", padx=40, pady=20, command=button_add,)
+button_7 = Button(root, text="7", padx=40, pady=20, command=button_add,)
+button_8 = Button(root, text="8", padx=40, pady=20, command=button_add,)
+button_9 = Button(root, text="9", padx=40, pady=20, command=button_add,)
+button_0 = Button(root, text="0", padx=40, pady=20, command=button_add,)
 
-# get problem
-num1 = int(input("Enter num1: "));
-operator= input("Enter operator (e.g. /): ");
-num2 = int(input("Enter num2: "));
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add,)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_add,)
+button_clear = Button(root, text="Clear", padx=79,
+                      pady=20, command=button_add,)
 
-if (operator == "*"):
-    res = num1 * num2;
-    print(f"That's {res}");
-elif (operator == "/"):
-    res = num1/num2;
-    print(f"That's {res}");
-elif (operator == "+"):
-    res = num1+num2;
-    print(f"That's {res}");
-elif (operator == "-"):
-    res = num1-num2;
-    print(f"That's {res}");
-else:
-    print("TRY AGAIN");
-    
+button_subtract = Button(root, text="-", padx=41, pady=20, command=button_add,)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=button_add,)
+button_divide = Button(root, text="/", padx=41, pady=20, command=button_add,)
+
+# Put the buttons on the screen
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
+root.mainloop()
